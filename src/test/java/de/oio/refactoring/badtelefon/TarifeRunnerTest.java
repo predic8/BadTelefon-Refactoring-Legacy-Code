0e0b6a2f3119a3c1ab67137f76ad7adcd9f4263f
Falk Sippach <falk.sippach@oio.de>
2016-03-03 18:37:56 +0100

    01-demo catching console and check for expected output

--- src/test/java/de/oio/refactoring/badtelefon/TarifeRunnerTest.java
+++ src/test/java/de/oio/refactoring/badtelefon/TarifeRunnerTest.java
@@ -1,14 +1,37 @@
 package de.oio.refactoring.badtelefon;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TarifeRunnerTest {
 
+	private PrintStream originalSysOut;
+	private ByteArrayOutputStream consoleStream;
+
+	@Before
+	public void init() {
+		originalSysOut = System.out;
+		consoleStream = new ByteArrayOutputStream();
+		PrintStream printStream = new PrintStream(consoleStream);
+		System.setOut(printStream);
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSimpleOutput() {
+		System.out.println("Hallo Publikum!");
+		System.out.print("Hallo Falk!");
+		assertEquals("Hallo Publikum!\r\nHallo Falk!", consoleStream.toString());
+	}
+
+	@After
+	public void teardown() {
+		System.setOut(originalSysOut);
 	}
 
 }
