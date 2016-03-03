1aefed5da7ba6bba4dc28e009f931dec8279ee2e
Falk Sippach <falk.sippach@oio.de>
2016-03-03 18:36:09 +0100

    01-demo empty TarifeRunnerTest added

---
+++ src/test/java/de/oio/refactoring/badtelefon/TarifeRunnerTest.java
@@ -0,0 +1,14 @@
+package de.oio.refactoring.badtelefon;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TarifeRunnerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
