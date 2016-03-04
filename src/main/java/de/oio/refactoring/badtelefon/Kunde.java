e5af0d6ed4ad9247a563d24aa2cf44304fd943d9
Falk Sippach <falk.sippach@oio.de>
2016-03-04 18:27:51 +0100

    demo-04: inlined temporary fields

--- src/main/java/de/oio/refactoring/badtelefon/Kunde.java
+++ src/main/java/de/oio/refactoring/badtelefon/Kunde.java
@@ -9,8 +9,7 @@ public class Kunde {
 	}
 
 	public void account(int minuten, int stunde, int minute) {
-		String message1 = String.format("Berechne Gespräch mit %02d min um %02d:%02d mit Tarif %s", minuten, stunde, minute, tarif.tarif);
-		writeToConsole(message1);
+		writeToConsole(String.format("Berechne Gespräch mit %02d min um %02d:%02d mit Tarif %s", minuten, stunde, minute, tarif.tarif));
 		double preis = 0;
 
 		boolean mondschein = isMondschein(stunde);
@@ -38,12 +37,10 @@ public class Kunde {
 			break;
 
 		}
-		String message2 = String.format("Preis für das Gespräch: %.2f", preis);
-		writeToConsole(message2);
+		writeToConsole(String.format("Preis für das Gespräch: %.2f", preis));
 		
 		gebuehr += preis;
-		String message3 = String.format("Gesamtgebühr nach Gespräch um %02d:%02d (Mondscheinzeit: %s): %.2f", stunde, minute, mondschein, gebuehr);
-		writeToConsole(message3);
+		writeToConsole(String.format("Gesamtgebühr nach Gespräch um %02d:%02d (Mondscheinzeit: %s): %.2f", stunde, minute, mondschein, gebuehr));
 	}
 
 	protected static void writeToConsole(String message1) {
