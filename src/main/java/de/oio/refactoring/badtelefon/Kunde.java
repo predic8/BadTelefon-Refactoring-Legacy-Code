beb1b23c5d5e8ac2ffea39cbadda81676fa18e86
Falk Sippach <falk.sippach@oio.de>
2016-03-05 08:43:32 +0100

    demo-05: moved writeToConsole to new class ConsoleOutputter

--- src/main/java/de/oio/refactoring/badtelefon/Kunde.java
+++ src/main/java/de/oio/refactoring/badtelefon/Kunde.java
@@ -1,5 +1,7 @@
 package de.oio.refactoring.badtelefon;
 
+import de.oio.refactoring.badtelefon.output.ConsoleOutputter;
+
 public class Kunde {
 	double gebuehr = 0.0;
 	Tarif tarif;
@@ -9,7 +11,7 @@ public class Kunde {
 	}
 
 	public void account(int minuten, int stunde, int minute) {
-		writeToConsole(String.format("Berechne Gespräch mit %02d min um %02d:%02d mit Tarif %s", minuten, stunde, minute, tarif.tarif));
+		ConsoleOutputter.writeToConsole(String.format("Berechne Gespräch mit %02d min um %02d:%02d mit Tarif %s", minuten, stunde, minute, tarif.tarif));
 		double preis = 0;
 
 		boolean mondschein = isMondschein(stunde);
@@ -37,14 +39,10 @@ public class Kunde {
 			break;
 
 		}
-		writeToConsole(String.format("Preis für das Gespräch: %.2f", preis));
+		ConsoleOutputter.writeToConsole(String.format("Preis für das Gespräch: %.2f", preis));
 		
 		gebuehr += preis;
-		writeToConsole(String.format("Gesamtgebühr nach Gespräch um %02d:%02d (Mondscheinzeit: %s): %.2f", stunde, minute, mondschein, gebuehr));
-	}
-
-	protected static void writeToConsole(String message1) {
-		System.out.println(message1);
+		ConsoleOutputter.writeToConsole(String.format("Gesamtgebühr nach Gespräch um %02d:%02d (Mondscheinzeit: %s): %.2f", stunde, minute, mondschein, gebuehr));
 	}
 
 	protected static boolean isMondschein(int stunde) {
---
+++ src/main/java/de/oio/refactoring/badtelefon/output/ConsoleOutputter.java
@@ -0,0 +1,9 @@
+package de.oio.refactoring.badtelefon.output;
+
+public class ConsoleOutputter {
+
+	public static void writeToConsole(String message) {
+		System.out.println(message);
+	}
+
+}
