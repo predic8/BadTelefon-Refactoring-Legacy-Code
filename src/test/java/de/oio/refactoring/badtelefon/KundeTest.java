f95d0732d01a34c489b780efe89aa6900b4d4738
Falk Sippach <falk.sippach@oio.de>
2016-03-07 12:14:43 +0100

    demo-08: replaced StringOutputter Dummy with Outputter mock

--- src/test/java/de/oio/refactoring/badtelefon/KundeTest.java
+++ src/test/java/de/oio/refactoring/badtelefon/KundeTest.java
@@ -1,23 +1,33 @@
 package de.oio.refactoring.badtelefon;
 
-import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import de.oio.refactoring.badtelefon.output.Outputter;
-import de.oio.refactoring.badtelefon.output.StringOutputter;
 
+@RunWith(MockitoJUnitRunner.class)
 public class KundeTest {
 
+	private Outputter outputterMock;
+
+	@Before
+	public void init() {
+		outputterMock = Mockito.mock(Outputter.class);
+	}
+
 	@Test
-	public void testAccount() {
-		Outputter stringOutputter = new StringOutputter();
-		Kunde kunde = new Kunde(Tarif.BUSINESS, stringOutputter);
-		kunde.account(10, 12, 35);
-		assertEquals(
-				"Berechne Gespräch mit 10 min um 12:35 mit Tarif 1\r\n" + "Preis für das Gespräch: 12,90\r\n"
-						+ "Gesamtgebühr nach Gespräch um 12:35 (Mondscheinzeit: false): 12,90\r\n",
-				stringOutputter.toString());
+	public void testAccountWithBusinessTarif() {
+		new Kunde(Tarif.BUSINESS, outputterMock).account(10, 12, 35);
+		verify(outputterMock).writeLine("Berechne Gespräch mit 10 min um 12:35 mit Tarif 1");
+		verify(outputterMock).writeLine("Preis für das Gespräch: 12,90");
+		verify(outputterMock).writeLine("Gesamtgebühr nach Gespräch um 12:35 (Mondscheinzeit: false): 12,90");
+		verifyNoMoreInteractions(outputterMock);
 	}
 
 }
