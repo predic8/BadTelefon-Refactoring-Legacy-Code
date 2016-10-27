fa73fcdc644a26155bdd8111e8e27183f7f8690f
Falk Sippach <fsippach@intern.oio.de>
2016-10-27 07:03:24 +0200

    Live Coding bei der JUG Saxony

--- src/main/java/de/oio/refactoring/badtelefon/Kunde.java
+++ src/main/java/de/oio/refactoring/badtelefon/Kunde.java
@@ -3,20 +3,23 @@ package de.oio.refactoring.badtelefon;
 public class Kunde {
 	double gebuehr = 0.0;
 	Tarif tarif;
+	private Outputter outputter = new ConsoleOutputter();
 
-	public Kunde(int tarifArt) {
+	public Kunde(int tarifArt, Outputter outputter) {
+		this(tarifArt);
+		this.outputter = outputter;
+	}
+	
+	private Kunde(int tarifArt) {
 		this.tarif = new Tarif(tarifArt);
 	}
 
 	public void account(int minuten, int stunde, int minute) {
 		String message1 = String.format("Berechne Gespräch mit %02d min um %02d:%02d mit Tarif %s", minuten, stunde, minute, tarif.tarif);
-		System.out.println(message1);
-		boolean mondschein = false;
+		outputter.writeLine(message1);
 		double preis = 0;
 
-		// Mondscheinzeit ?
-		if (stunde < 9 || stunde > 18)
-			mondschein = true;
+		boolean mondschein = isMondschein(stunde);
 
 		// Gespraechspreis ermitteln
 		switch (tarif.tarif) {
@@ -41,12 +44,14 @@ public class Kunde {
 			break;
 
 		}
-		String message2 = String.format("Preis für das Gespräch: %.2f", preis);
-		System.out.println(message2);
+		outputter.writeLine(String.format("Preis für das Gespräch: %.2f", preis));
 		
 		gebuehr += preis;
-		String message3 = String.format("Gesamtgebühr nach Gespräch um %02d:%02d (Mondscheinzeit: %s): %.2f", stunde, minute, mondschein, gebuehr);
-		System.out.println(message3);
+		outputter.writeLine(String.format("Gesamtgebühr nach Gespräch um %02d:%02d (Mondscheinzeit: %s): %.2f", stunde, minute, mondschein, gebuehr));
+	}
+
+	private static boolean isMondschein(int stunde) {
+		return stunde < 9 || stunde > 18;
 	}
 
 	public double getGebuehr() {
