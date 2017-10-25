f572f4315d573dde75cd4e501b0fd1c113d0924e
Falk Sippach <falk@jug-da.de>
2017-10-25 15:33:35 +0200

    Schritt 1 bis 4, vorgestellt auf der JCON 2017

--- src/main/java/de/oio/refactoring/badtelefon/Kunde.java
+++ src/main/java/de/oio/refactoring/badtelefon/Kunde.java
@@ -10,27 +10,22 @@ public class Kunde {
 
 	public void account(int minuten, int stunde, int minute) {
 		String message1 = String.format("Berechne Gespräch mit %02d min um %02d:%02d mit Tarif %s", minuten, stunde, minute, tarif.tarif);
-		System.out.println(message1);
-		boolean mondschein = false;
+		writeToConsole(message1);
 		double preis = 0;
-
-		// Mondscheinzeit ?
-		if (stunde < 9 || stunde > 18)
-			mondschein = true;
 
 		// Gespraechspreis ermitteln
 		switch (tarif.tarif) {
 		case Tarif.PRIVAT:
 			minuten = minuten - 1;
 			minuten = minuten < 0 ? 0 : minuten;
-			if (mondschein)
+			if (isMondschein(stunde))
 				preis = minuten * 0.69;
 			else
 				preis = minuten * 1.99;
 			break;
 
 		case Tarif.BUSINESS:
-			if (mondschein)
+			if (isMondschein(stunde))
 				preis = minuten * 0.79;
 			else
 				preis = minuten * 1.29;
@@ -42,11 +37,19 @@ public class Kunde {
 
 		}
 		String message2 = String.format("Preis für das Gespräch: %.2f", preis);
-		System.out.println(message2);
+		writeToConsole(message2);
 		
 		gebuehr += preis;
-		String message3 = String.format("Gesamtgebühr nach Gespräch um %02d:%02d (Mondscheinzeit: %s): %.2f", stunde, minute, mondschein, gebuehr);
-		System.out.println(message3);
+		String message3 = String.format("Gesamtgebühr nach Gespräch um %02d:%02d (Mondscheinzeit: %s): %.2f", stunde, minute, isMondschein(stunde), gebuehr);
+		writeToConsole(message3);
+	}
+
+	public void writeToConsole(String message1) {
+		System.out.println(message1);
+	}
+
+	public static boolean isMondschein(int stunde) {
+		return stunde < 9 || stunde > 18;
 	}
 
 	public double getGebuehr() {
